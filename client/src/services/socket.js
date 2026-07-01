f8eb6e57a215375e101994fa6132d8f8a00d2fe6
Shashank <jhashashank3747@gmail.com>
2026-07-02 03:37:08 +0530

    Use environment variables for API and socket URLs

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000', {
   withCredentials: true,
-  autoConnect: false, // we manually connect when needed
+  autoConnect: false,
 });
 
 export default socket;
